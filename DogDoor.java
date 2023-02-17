76dc137398e69e1b3742ca60b3ee14b2cdccccee
hindawi <ahmedhindawi2001@gmail.com>
2023-02-17 08:37:10 +0200

    fixed indentation for DogDoor

--- DogDoor.java
+++ DogDoor.java
@@ -1,17 +1,21 @@
 public class DogDoor {
- private boolean open;
- public DogDoor() {
- this.open = false;
- }
- public void open() {
- System.out.println(“The dog door opens.”);
- open = true;
- }
- public void close() {
- System.out.println(“The dog door closes.”);
- open = false;
- }
- public boolean isOpen() {
- return open;
- }
+    private boolean open;
+
+    public DogDoor() {
+        this.open = false;
+    }
+
+    public void open() {
+        System.out.println("The dog door opens");
+        open = true;
+    }
+
+    public void close() {
+        System.out.println("The dog door closes.");
+        open = false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
 }
